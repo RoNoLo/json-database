e0c21de4d34616bf495cf81fb811ae618eb1f275
Ronald Locke <scm@kakaobart.de>
2019-11-10 22:53:05 +0100

    rewriting the conditions into a series of closures, which call the tree on the fly.

--- tests/src/query/conditionals/QueryConditionalsWithMultipleFieldsTest.php
+++ tests/src/query/conditionals/QueryConditionalsWithMultipleFieldsTest.php
@@ -44,8 +44,8 @@ class QueryConditionalsWithMultipleFieldsTest extends QueryTestBase
         $result = $query
             ->find([
                 "registered" => [
-                    '$gt' => new \DateTime("2018-01-01T10:00:00"),
-                    '$lt' => new \DateTime("2020-01-01T10:00:00"),
+                    '$gt' => '$DateTime:2018-01-01T10:00:00',
+                    '$lt' => '$DateTime:2020-01-01T10:00:00',
                 ],
             ])
             ->execute()
